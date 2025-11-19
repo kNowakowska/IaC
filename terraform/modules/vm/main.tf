ccd8a4eb9e72463a202fb9276adab7045230935a
Kinga Nowakowska <knowakowska@macbook-pro-kinga.home>
2025-11-19 17:43:04 +0100

    Add invalid vm

--- terraform/modules/vm/main.tf
+++ terraform/modules/vm/main.tf
@@ -42,41 +42,41 @@ resource "google_compute_instance" "vm2" {
   tags                    = ["http-server"]
 }
 
-# resource "google_compute_instance" "vm3" {
-#   name         = "${var.name}-3"
-#   machine_type = var.machine_type
-#   zone         = "${var.region}-a"
-#   boot_disk {
-#     initialize_params {
-#       image = var.image
-#     }
-#   }
-#   network_interface {
-#     subnetwork = var.subnet
-#     access_config {}
-#   }
-#   metadata_startup_script = <<-EOT
-#     #!/bin/bash
-#     apt-get update
-#     apt-get install -y nginx
-#   EOT
-#   tags                    = ["http-server"]
-# }
+resource "google_compute_instance" "vm3" {
+  name         = "${var.name}-3"
+  machine_type = var.machine_type
+  zone         = "${var.region}-a"
+  boot_disk {
+    initialize_params {
+      image = var.image
+    }
+  }
+  network_interface {
+    subnetwork = var.subnet
+    access_config {}
+  }
+  metadata_startup_script = <<-EOT
+    #!/bin/bash
+    apt-get update
+    apt-get install -y nginx
+  EOT
+  tags                    = ["http-server"]
+}
 
 
 
 
-# resource "google_compute_instance" "invalid" {
-#   name         = "fail-test"
-#   machine_type = "INVALID_TYPE" # <-- This will fail
-#   zone         = "us-central1-a"
-#   boot_disk {
-#     initialize_params {
-#       image = var.image
-#     }
-#   }
-#   network_interface {
-#     subnetwork = var.subnet
-#     access_config {}
-#   }
-# }
+resource "google_compute_instance" "invalid" {
+  name         = "fail-test"
+  machine_type = "INVALID_TYPE" # <-- This will fail
+  zone         = "us-central1-a"
+  boot_disk {
+    initialize_params {
+      image = var.image
+    }
+  }
+  network_interface {
+    subnetwork = var.subnet
+    access_config {}
+  }
+}
